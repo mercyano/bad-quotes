9cdf51d9b3149bca259c196f7e97b64173ebf316
Mercyano Dandi Hidayat <mercyanodandi0@gmail.com>
2023-04-13 07:43:40 +0700

    GET request from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import Header from "./components/Header";
-import Quotes from "./components/Quotes";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header';
+import Quotes from './components/Quotes';
 
 const App = () => {
-  return (
-    <div className="font-courier">
-      <Header></Header>
-      <Quotes></Quotes>
-    </div>
-  );
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://api.breakingbadquotes.xyz/v1/quotes')
+			.then((res) => setData(res.data))
+			.catch((err) => console.error(err));
+	}, []);
+
+	return (
+		<div className="font-courier">
+			<Header />
+			<Quotes data={data} />
+		</div>
+	);
 };
 
 export default App;
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import hero from '../assets/breaking-bad-hero.png';
 
-const Quotes = () => {
+const Quotes = (props) => {
 	return (
 		// Flex Container
 		<div className="flex flex-col md:flex-row justify-center items-center mt-16 md:mt-20 px-8">
 			{/* Quotes */}
-			<div className="container pl-4 pr-4 border-l-4 border-white md:ml-16 md:mb-10">
-				<p className="text-white md:text-lg lg:text-2xl">
-					<em>
-						Sitting around, smoking marijuana, eating Cheetos and masturbating
-						do not constitute 'plans.'
-					</em>
-				</p>
-				<span className="text-gray-400 md:text-lg lg:text-2xl">
-					<em>- Walter White</em>
-				</span>
-			</div>
+
+			{props.data.length > 0 && (
+				<div className="container pl-4 pr-4 border-l-4 border-white md:ml-16 md:mb-10">
+					<p className="text-white md:text-lg lg:text-2xl">
+						<em>{props.data[0].quote}</em>
+					</p>
+					<span className="text-gray-400 md:text-lg lg:text-2xl">
+						<em>- {props.data[0].author}</em>
+					</span>
+				</div>
+			)}
+
 			{/* Image */}
 			<div className="flex mt-8 md:mt-0 mx-auto justify-center items-center">
 				<img className="md:w-full" src={hero}></img>
